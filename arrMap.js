30975ceea3c42d85e750a119e4ce3cead741f997
Carmen Shing <carmenshing@gmail.com>
2018-06-07 21:20:50 +0000

    added arrMap function

---
+++ arrMap.js
@@ -0,0 +1,15 @@
+var input = [
+    { x: 3, y: 4 },
+    { x: 12, y: 5 },
+    { x: 8, y: 15 }
+  ];
+
+  var result = input.map(obj => Math.sqrt(obj.x * obj.x + obj.y * obj.y));
+  
+  console.log(result[0] === 5);
+  console.log(result[1] === 13);
+  console.log(result[2] === 17);
+
+// console.log(result[0]);
+// console.log(result[1]);
+// console.log(result[2]);
